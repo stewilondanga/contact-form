17344c20813ac84c944bc28471b715aaea0adbe2
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 11:31:53 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Auto resize input
+function resizeInput() {
+    $(this).attr('size', $(this).val().length);
+}
+
+
 var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
