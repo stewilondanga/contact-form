116827a551354a233cacd20d7e6938442e244d15
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 11:34:45 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,33 @@ console.clear();
 			 }
 		 };
 
-		 
+		 // IE7 support
+  if ( !document.querySelectorAll ) {
+
+    function getElementsByClass(searchClass,node,tag) {
+      var classElements = new Array();
+      node = node || document;
+      tag = tag || '*';
+      var els = node.getElementsByTagName(tag);
+      var elsLen = els.length;
+      var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+      for (i = 0, j = 0; i < elsLen; i++) {
+        if ( pattern.test(els[i].className) ) {
+          classElements[j] = els[i];
+          j++;
+        }
+      }
+      return classElements;
+    }
+
+    textareas = getElementsByClass('expanding');
+  }
+
+  for (var i = 0; i < textareas.length; i++ ) {
+    attachResize(textareas[i]);
+  }
+
+})();
 
 var navigate = (function() {
 	$('.dd').toggle();
